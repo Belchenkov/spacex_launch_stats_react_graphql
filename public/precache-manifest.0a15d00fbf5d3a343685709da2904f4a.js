b4d18b5a3a052519178ea118fcdbed8143d2d190
Алексей Бельченков <u608110@gmail.com>
2018-12-20 14:33:23 +0400

    Deploy to Heroku

---
+++ public/precache-manifest.0a15d00fbf5d3a343685709da2904f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "adf0e1a7679f4f954e16",
+    "url": "/static/css/main.8c59ed4a.chunk.css"
+  },
+  {
+    "revision": "adf0e1a7679f4f954e16",
+    "url": "/static/js/main.adf0e1a7.chunk.js"
+  },
+  {
+    "revision": "c60f0a6986772ac3d97a",
+    "url": "/static/js/1.c60f0a69.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "eb49ff83e63bf62aeb87c74a3df6fca6",
+    "url": "/index.html"
+  }
+];
